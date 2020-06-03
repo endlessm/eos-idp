b54e38febacad5e55f9e1c804c2af09e82fd2ff6
Dan Nicholson <nicholson@endlessm.com>
2020-06-03 16:34:32 -0600

    Remove old login and logout views
    We're using allauth's views now.

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
@@ -17,16 +16,3 @@ def profile(request):
         'title': _('Profile'),
     }
     return render(request, 'main/profile.html', context=context)
-
-
-class LoginView(auth_views.LoginView):
-    template_name = 'main/login.html'
-
-    # For some reason, LoginView doesn't set a title in the context
-    extra_context = {
-        'title': _('Login'),
-    }
-
-
-class LogoutView(auth_views.LogoutView):
-    template_name = 'main/logout.html'
