dae6ba4dce1e8637371efef04e78bcf7a8da2461
Dan Nicholson <nicholson@endlessm.com>
2020-06-09 15:13:46 -0600

    Configure static files for collection
    Collect static files to the staticfiles directory and ignore it.

--- eosidp/settings.py
+++ eosidp/settings.py
@@ -165,6 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Load local settings
