0337776efb58d25f7583b9714e28823d1d652e63
Dan Nicholson <nicholson@endlessm.com>
2020-08-10 15:12:23 -0600

    Include standard name claim
    oidc_provider does not provide this by default. `get_full_name()` is
    simply the concatentation of the user's first and last name.

--- main/utils.py
+++ main/utils.py
@@ -1,5 +1,7 @@
 def oidc_userinfo(claims, user):
     """Add claims to OpenID Connect userinfo"""
+    claims['name'] = user.get_full_name()
+
     # Tell relying parts to re-use the local username if possible
     claims['preferred_username'] = user.get_username()
 
